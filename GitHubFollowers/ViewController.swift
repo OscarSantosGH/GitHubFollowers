40be5ec8d5309dc9106baecb84fff11e83fbbba8
Oscar Santos <oscarsantos.go@gmail.com>
2019-12-28 18:36:51 -0500

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Oscar Santos on 12/28/19.
+//  Copyright © 2019 Oscar Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
