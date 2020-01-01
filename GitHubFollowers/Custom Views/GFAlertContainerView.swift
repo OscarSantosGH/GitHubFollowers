3a98bf6a5f1ae4f2133efb2f49b3ca8477842ec9
Oscar Santos <oscarsantos.go@gmail.com>
2020-01-01 12:50:36 -0500

    added: GFAlertContainerView

---
+++ GitHubFollowers/Custom Views/GFAlertContainerView.swift
@@ -0,0 +1,30 @@
+//
+//  GFAlertContainerView.swift
+//  GitHubFollowers
+//
+//  Created by Oscar Santos on 1/1/20.
+//  Copyright © 2020 Oscar Santos. All rights reserved.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure(){
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
