82a52773a52ec013ef667bdd207edf889fc789f5
Oscar Santos <oscarsantos.go@gmail.com>
2020-01-05 00:29:31 -0500

    added: FollowerCell and GFAvatarImageView

---
+++ GitHubFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,31 @@
+//
+//  GFAvatarImageView.swift
+//  GitHubFollowers
+//
+//  Created by Oscar Santos on 1/4/20.
+//  Copyright © 2020 Oscar Santos. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure(){
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
