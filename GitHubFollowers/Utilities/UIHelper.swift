5269edb493d00bd86c9fc3790afa538595885c76
Oscar Santos <oscarsantos.go@gmail.com>
2020-01-07 22:38:10 -0500

    mod: Memory Management improved

---
+++ GitHubFollowers/Utilities/UIHelper.swift
@@ -0,0 +1,27 @@
+//
+//  UIHelper.swift
+//  GitHubFollowers
+//
+//  Created by Oscar Santos on 1/7/20.
+//  Copyright © 2020 Oscar Santos. All rights reserved.
+//
+
+import UIKit
+
+struct UIHelper {
+    
+    static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimunItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimunItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+    
+}
