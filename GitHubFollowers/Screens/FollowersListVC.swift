b0d33a48fd42bafcd19be6682a1a702027ff1ca3
Oscar Santos <oscarsantos.go@gmail.com>
2020-01-07 21:35:37 -0500

    added: Initial UICollectionView setup

--- GitHubFollowers/Screens/FollowersListVC.swift
+++ GitHubFollowers/Screens/FollowersListVC.swift
@@ -11,12 +11,48 @@ import UIKit
 class FollowersListVC: UIViewController {
     
     var username: String!
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    func configureViewController(){
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+
+    }
+    
+    func configureCollectionView(){
+        collectionView = UICollectionView(frame: view.frame, collectionViewLayout: createThreeColumnFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimunItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimunItemSpacing * 2)
+        let itemWidth = availableWidth / 3
         
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+    
+    func getFollowers(){
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
             
             switch result {
@@ -28,10 +64,6 @@ class FollowersListVC: UIViewController {
             }
             
         }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        navigationController?.setNavigationBarHidden(false, animated: true)
     }
 
 }
