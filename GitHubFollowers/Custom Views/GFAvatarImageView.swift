0cbb71067676f7f3703a3c891a3b9c92ab22b33a
Oscar Santos <oscarsantos.go@gmail.com>
2020-01-08 23:17:34 -0500

    added: NSCache to save the avatar image once downloaded

--- GitHubFollowers/Custom Views/GFAvatarImageView.swift
+++ GitHubFollowers/Custom Views/GFAvatarImageView.swift
@@ -11,7 +11,7 @@ import UIKit
 class GFAvatarImageView: UIImageView {
     
     let placeholderImage = UIImage(named: "avatar-placeholder")!
-
+    let cache = NetworkManager.shared.cache
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
@@ -29,6 +29,14 @@ class GFAvatarImageView: UIImageView {
     }
     
     func downloadImage(from urlString:String){
+        
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey){
+            self.image = image
+            return
+        }
+        
         guard let url = URL(string: urlString) else { return }
         
         let task = URLSession.shared.dataTask(with: url) {[weak self] (data, response, error) in
@@ -40,6 +48,7 @@ class GFAvatarImageView: UIImageView {
             guard let data = data else { return }
             
             guard let image = UIImage(data: data) else { return }
+            self.cache.setObject(image, forKey: cacheKey)
             
             DispatchQueue.main.async {
                 self.image = image
