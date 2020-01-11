18a652f6d3a3869c838c6a8e6c9f869312279d3e
Oscar Santos <oscarsantos.go@gmail.com>
2020-01-11 11:02:46 -0500

    added: LoadingView on FollowersListVC

--- GitHubFollowers/Extensions/UIViewController+Ext.swift
+++ GitHubFollowers/Extensions/UIViewController+Ext.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
+fileprivate var containerView:UIView!
+
 extension UIViewController{
+    
     func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String){
         DispatchQueue.main.async {
             let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
             alertVC.modalPresentationStyle = .overFullScreen
             alertVC.modalTransitionStyle = .crossDissolve
             self.present(alertVC, animated: true)
+        }
+    }
+    
+    func showLoadingView(){
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.30) {
+            containerView.alpha = 0.8
+        }
+        
+        let activityIndicador = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicador)
+        
+        activityIndicador.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicador.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicador.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        activityIndicador.startAnimating()
+    }
+    
+    func dismissLoadingView(){
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
