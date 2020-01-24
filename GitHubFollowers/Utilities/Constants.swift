a0ff6b1048b7fd759b032a7f8a3066b4938560f5
Oscar Santos <oscarsantos.go@gmail.com>
2020-01-23 22:22:27 -0500

    added: Constants, GFSecondaryTitleLabel and GFUserInfoHeaderVC

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Oscar Santos on 1/23/20.
+//  Copyright © 2020 Oscar Santos. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols{
+    static let location = "mappin.and.ellipse"
+}
