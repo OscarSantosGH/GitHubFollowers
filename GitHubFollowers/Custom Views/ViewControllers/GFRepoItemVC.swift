2be8a65425649ba9cb5dcb55c4bc58f60eff9ec1
Oscar Santos <oscarsantos.go@gmail.com>
2020-01-25 17:34:57 -0500

    added: GFRepoItemVC

---
+++ GitHubFollowers/Custom Views/ViewControllers/GFRepoItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  GFRepoItemVC.swift
+//  GitHubFollowers
+//
+//  Created by Oscar Santos on 1/25/20.
+//  Copyright © 2020 Oscar Santos. All rights reserved.
+//
+
+import UIKit
+
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func configureItems(){
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+}
